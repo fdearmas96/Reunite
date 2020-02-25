530a230dd428792e81ef6235937b70db73136936
fdearmas96 <47925506+fdearmas96@users.noreply.github.com>
2020-02-24 22:12:31 -0300

    Create ConsultaUsuarioLogueado.java

---
+++ app/src/main/java/com/example/reunite/ConsultaUsuarioLogueado.java
@@ -0,0 +1,30 @@
+package com.example.reunite;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.fragment.app.Fragment;
+
+public class ConsultaUsuarioLogueado    {
+    private String user="";
+    private String pass="";
+    //Context context = getActivity();
+
+
+    public String getPass(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences("Credenciales", Context.MODE_PRIVATE);
+        String pass = preferences.getString("pass","");
+        pass ="root";
+        return pass;
+    }
+
+    public String getUser(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences("Credenciales", Context.MODE_PRIVATE);
+        String user = preferences.getString("user","");
+        //user = "Usuario";
+        return user;
+    }
+
+
+}
