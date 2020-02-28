93d74c619b331611514a268f16ae66ce769a688c
fdearmas96 <47925506+fdearmas96@users.noreply.github.com>
2020-02-28 14:32:16 -0300

    Update ImplementsFragments.java

--- app/src/main/java/com/example/reunite/ImplementsFragments.java
+++ app/src/main/java/com/example/reunite/ImplementsFragments.java
@@ -1,7 +1,7 @@
 package com.example.reunite;
 
 public interface ImplementsFragments extends FragmentInicio.OnFragmentInteractionListener, PublicacionFragment.OnFragmentInteractionListener,
-        NuevaPublicacionFragment.OnFragmentInteractionListener, LoguinFragment.OnFragmentInteractionListener
+        NuevaPublicacionFragment.OnFragmentInteractionListener, LoguinFragment.OnFragmentInteractionListener, ChatFerFragment.OnFragmentInteractionListener
 
 
 {
