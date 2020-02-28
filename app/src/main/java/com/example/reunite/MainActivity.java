ea00620f647613b6ab90c3d5398056352f740947
fdearmas96 <47925506+fdearmas96@users.noreply.github.com>
2020-02-28 14:32:26 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/reunite/MainActivity.java
+++ app/src/main/java/com/example/reunite/MainActivity.java
@@ -72,10 +72,10 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                //Fragment fragment = new NuevaPublicacionFragment();
-                //llamar_fragment(fragment);
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+                Fragment fragment = new ChatFerFragment();
+                llamar_fragment(fragment);
 
             }
         });
