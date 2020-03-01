5dd015321d2b8d0af4a90ceb6c3158f1e025f0af
fdearmas96 <47925506+fdearmas96@users.noreply.github.com>
2020-03-01 09:05:34 -0300

    Update ChatFerFragment.java

--- app/src/main/java/com/example/reunite/ChatFerFragment.java
+++ app/src/main/java/com/example/reunite/ChatFerFragment.java
@@ -10,8 +10,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import com.example.reunite.classes.Utilidades;
 
 import java.util.ArrayList;
 
