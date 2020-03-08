edfb48d9f5980442f73a5eef4664b0930af1b2ea
metroid112 <federico.aguilera112@gmail.com>
2020-03-08 20:51:02 -0300

    Layout chat

---
+++ app/src/main/java/com/example/reunite/fragments/Chat.java
@@ -0,0 +1,30 @@
+package com.example.reunite.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class Chat extends Fragment {
+    private String mParam1;
+    private String mParam2;
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    public static Chat newInstance(String param1, String param2) {
+        Chat fragment = new Chat();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+}
